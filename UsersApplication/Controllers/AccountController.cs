423d81df948369b3b1668f61c3a8937b585aca94
qiperman <matveevvitya7@yandex.ru>
2017-11-19 13:18:00 +0300

    AddLogin

---
+++ UsersApplication/Controllers/AccountController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using UsersApplication.Models;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+
+namespace UsersApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Аccount/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private UsersApplicationManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<UsersApplicationManager>();
+            }
+        }
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        //
+        //Get: /Account/Register/
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        //Post: /Account/Register/
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                MyUser user = new MyUser { UserName = model.Login, Email = model.Email };
+                IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+
+        //
+        //Get: /Account/Login/
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        //Post: /Account/Login/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                MyUser user = await UserManager.FindAsync(model.Login, model.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Неверный логин или пароль.");
+                }
+                else
+                {
+                    ClaimsIdentity claim = await UserManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    AuthenticationManager.SignOut();
+                    AuthenticationManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = true
+                    }, claim);
+                    if (String.IsNullOrEmpty(returnUrl))
+                        return RedirectToAction("Index", "Home");
+                    return Redirect(returnUrl);
+                }
+            }
+            ViewBag.returnUrl = returnUrl;
+            return View(model);
+        }
+
+        //
+        //Get: /Account/Logout
+        public ActionResult Logout()
+        {
+            AuthenticationManager.SignOut();
+            return RedirectToAction("Login");
+        }
+
+	}
+}
