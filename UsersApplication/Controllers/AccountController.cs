b25b70549c9213d16395ba3df25ffbd869a12fc5
qiperman <matveevvitya7@yandex.ru>
2017-11-19 17:51:47 +0300

    BackUp

--- UsersApplication/Controllers/AccountController.cs
+++ UsersApplication/Controllers/AccountController.cs
@@ -9,6 +9,8 @@ using UsersApplication.Models;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using System.Security.Claims;
+using Microsoft.Owin.Security.DataProtection;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace UsersApplication.Controllers
 {
@@ -54,6 +56,8 @@ namespace UsersApplication.Controllers
                 IdentityResult result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+
+                    SendConfirmEmail(user.Id);
                     return RedirectToAction("Login", "Account");
                 }
                 else
@@ -100,7 +104,7 @@ namespace UsersApplication.Controllers
                         IsPersistent = true
                     }, claim);
                     if (String.IsNullOrEmpty(returnUrl))
-                        return RedirectToAction("Index", "Home");
+                        return RedirectToAction("Index", "Account");
                     return Redirect(returnUrl);
                 }
             }
@@ -116,5 +120,115 @@ namespace UsersApplication.Controllers
             return RedirectToAction("Login");
         }
 
+        [HttpGet]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> SendConfirmEmail(string userId)
+        {
+            MyUser user = await UserManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var provider = new DpapiDataProtectionProvider("Sample");
+
+                var userManager = new UserManager<MyUser>(new UserStore<MyUser>());
+
+                UserManager.UserTokenProvider = new DataProtectorTokenProvider<MyUser>(
+                    provider.Create("EmailConfirmation"));
+
+                var code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                // создаем ссылку для подтверждения
+                var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code },
+                           protocol: Request.Url.Scheme);
+                // отправка письма
+                await UserManager.SendEmailAsync(user.Id, "Подтверждение электронной почты", "Для завершения регистрации перейдите по ссылке:: <a href=\"" + callbackUrl + "\">завершить регистрацию</a>");
+                ViewBag.email = user.Email;
+                return View();
+            }
+            return RedirectToAction("Index", "Account");
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<ActionResult> DeleteConfirmed()
+        {
+            MyUser user = await UserManager.FindByNameAsync(User.Identity.Name);
+            if (user != null)
+            {
+                IdentityResult result = await UserManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Logout", "Account");
+                }
+            }
+            return RedirectToAction("Index", "Account");
+        }
+
+        public async Task<ActionResult> Edit()
+        {
+            MyUser user = await UserManager.FindByNameAsync(User.Identity.Name);
+            if (user != null)
+            {
+                EditModel model = new EditModel { FirstName = user.FirstName, SecondName = user.SecondName, Patronymic = user.Patronymic, Email = user.Email, Login = user.UserName, EmailConfirmed = user.EmailConfirmed, UserId = user.Id};
+                return View(model);
+            }
+            return RedirectToAction("Login", "Account");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(EditModel model)
+        {
+            MyUser user = await UserManager.FindByNameAsync(User.Identity.Name);
+            if (user != null)
+            {
+                user.FirstName = model.FirstName;
+                user.SecondName = model.SecondName;
+                user.Patronymic = model.Patronymic;
+                if (user.Email != model.Email)
+                {
+                    user.Email = model.Email;
+                    user.EmailConfirmed = false;
+                }
+                
+                IdentityResult result = await UserManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Account");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Что-то пошло не так");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Пользователь не найден");
+            }
+
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        public ActionResult ConfirmEmail(string userId, string code)
+        {
+            var provider = new DpapiDataProtectionProvider("Sample");
+
+            UserManager.UserTokenProvider = new DataProtectorTokenProvider<MyUser>(
+                provider.Create("EmailConfirmation"));
+            IdentityResult result = UserManager.ConfirmEmail(userId, code);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Account");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Что-то пошло не так");
+            }
+            return RedirectToAction("Index", "Account");
+
+        }
 	}
 }
--- UsersApplication/Models/UsersApplicationManager.cs
+++ UsersApplication/Models/UsersApplicationManager.cs
@@ -2,9 +2,13 @@
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.DataProtection;
+using Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace UsersApplication.Models
@@ -13,12 +17,14 @@ namespace UsersApplication.Models
     {
         public UsersApplicationManager(IUserStore<MyUser> store)  : base(store) 
         {
+
         }
 
         public static UsersApplicationManager Create(IdentityFactoryOptions<UsersApplicationManager> options, IOwinContext context) 
         {
             AppContext db = context.Get<AppContext>();
             UsersApplicationManager manager = new UsersApplicationManager(new UserStore<MyUser>(db));
+            manager.EmailService = new EmailService();
             manager.UserValidator = new UserValidator<MyUser>(manager)
             {
                 AllowOnlyAlphanumericUserNames = false,
@@ -26,5 +32,31 @@ namespace UsersApplication.Models
             };
             return manager;
         }
+    }
+}
+
+public class EmailService : IIdentityMessageService
+{
+    public Task SendAsync(IdentityMessage message)
+    {
+        // настройка логина, пароля отправителя
+        var from = "iw1v4n@yandex.ru";
+        var pass = "654321Qwerty";
+
+        // адрес и порт smtp-сервера, с которого мы и будем отправлять письмо
+        SmtpClient client = new SmtpClient("smtp.yandex.ru", 25);
+
+        client.DeliveryMethod = SmtpDeliveryMethod.Network;
+        client.UseDefaultCredentials = false;
+        client.Credentials = new System.Net.NetworkCredential(from, pass);
+        client.EnableSsl = true;
+
+        // создаем письмо: message.Destination - адрес получателя
+        var mail = new MailMessage(from, message.Destination);
+        mail.Subject = message.Subject;
+        mail.Body = message.Body;
+        mail.IsBodyHtml = true;
+
+        return client.SendMailAsync(mail);
     }
 }
